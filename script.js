61958b6209d714db7ecced8a6eb332b4decdb168
Komalsingh003 <153943927+Komalsingh003@users.noreply.github.com>
2026-01-08 12:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 function insert_Row() {
     //Write your code here	
-    const table = document.getElementById("sampleTable");
+    var table = document.getElementById("sampleTable");
+    var row = table.insertRow(0);
 
-    const newRow = table.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
 
-    // cells
-    const cell1 = newRow.insertCell(0);
-    const cell2 = newRow.insertCell(1);
-
-    // cell values
-    cell1.innerHTML = "New Cell1";
-    cell2.innerHTML = "New Cell2";
-}
-  
+    cell1.innerText = "New Cell1";
+    cell2.innerText = "New Cell2";
 }
